9bb954351976e3d876c0cb0f41197dececd59c51
Dmitriy Barbul <dimabarbul@gmail.com>
2024-07-14 13:29:01 +0300

    Refactor tests

---
+++ src/test/java/io/github/dimabarbul/WiremockOpenapiValidationExtension/RequestBuilder.java
@@ -0,0 +1,50 @@
+package io.github.dimabarbul.WiremockOpenapiValidationExtension;
+
+import java.nio.charset.StandardCharsets;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.http.ImmutableRequest;
+import com.github.tomakehurst.wiremock.http.Request;
+import com.github.tomakehurst.wiremock.http.RequestMethod;
+import com.google.common.net.MediaType;
+
+class RequestBuilder {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public static Request postJsonRequest(final String url, final Object body) {
+        try {
+            return ImmutableRequest.create()
+                    .withMethod(RequestMethod.POST)
+                    .withAbsoluteUrl(url)
+                    .withHeader("Content-Type", MediaType.JSON_UTF_8.toString())
+                    .withBody(mapper.writeValueAsBytes(body))
+                    .build();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Request postJsonRequest(final String url, final String body) {
+            return ImmutableRequest.create()
+                    .withMethod(RequestMethod.POST)
+                    .withAbsoluteUrl(url)
+                    .withHeader("Content-Type", MediaType.JSON_UTF_8.toString())
+                    .withBody(body.getBytes(StandardCharsets.UTF_8))
+                    .build();
+    }
+
+    public static Request getRequest(final String url) {
+        return ImmutableRequest.create()
+                .withMethod(RequestMethod.GET)
+                .withAbsoluteUrl(url)
+                .build();
+    }
+
+    public static Request deleteRequest(final String url) {
+        return ImmutableRequest.create()
+                .withMethod(RequestMethod.DELETE)
+                .withAbsoluteUrl(url)
+                .build();
+    }
+}
--- src/test/java/io/github/dimabarbul/WiremockOpenapiValidationExtension/ValidationResponseTransformerTest.java
+++ src/test/java/io/github/dimabarbul/WiremockOpenapiValidationExtension/ValidationResponseTransformerTest.java
@@ -1,19 +1,24 @@
 package io.github.dimabarbul.WiremockOpenapiValidationExtension;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.created;
 import static com.github.tomakehurst.wiremock.client.WireMock.delete;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.jsonResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.noContent;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static io.github.dimabarbul.WiremockOpenapiValidationExtension.RequestBuilder.deleteRequest;
+import static io.github.dimabarbul.WiremockOpenapiValidationExtension.RequestBuilder.getRequest;
+import static io.github.dimabarbul.WiremockOpenapiValidationExtension.RequestBuilder.postJsonRequest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,11 +27,10 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 import com.atlassian.oai.validator.OpenApiInteractionValidator;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.direct.DirectCallHttpServer;
 import com.github.tomakehurst.wiremock.direct.DirectCallHttpServerFactory;
-import com.github.tomakehurst.wiremock.http.ImmutableRequest;
 import com.github.tomakehurst.wiremock.http.MultiValue;
-import com.github.tomakehurst.wiremock.http.RequestMethod;
 import com.github.tomakehurst.wiremock.http.Response;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import com.github.tomakehurst.wiremock.matching.UrlPattern;
@@ -34,8 +38,7 @@ import com.google.common.net.MediaType;
 
 class ValidationResponseTransformerTest {
 
-    public static final String CONTENT_TYPE_HEADER = "Content-Type";
-
+    private static final int DEFAULT_VALIDATION_FAILURE_STATUS_CODE = 500;
     private static final String GET_USERS_URL = "/users";
     private static final String ADD_USER_URL = "/users";
     private static final String DELETE_USER_URL = "/users/123";
@@ -50,9 +53,7 @@ class ValidationResponseTransformerTest {
 
     @RegisterExtension
     private static final WireMockExtension wm = WireMockExtension.newInstance()
-            .options(wireMockConfig()
-                    .httpServerFactory(factory)
-                    .extensions(new ValidationResponseTransformer()))
+            .options(getWireMockConfiguration())
             .failOnUnmatchedRequests(false)
             .build();
 
@@ -71,40 +72,26 @@ class ValidationResponseTransformerTest {
     @Test
     void testRequestBodyHasRequiredProperties() {
         wm.stubFor(post(ADD_USER_URL)
-                .willReturn(aResponse()
-                        .withStatus(201)));
+                .willReturn(created()));
 
-        Response response = server.stubRequest(ImmutableRequest.create()
-                .withMethod(RequestMethod.POST)
-                .withAbsoluteUrl(wm.url(ADD_USER_URL))
-                .withHeader(CONTENT_TYPE_HEADER, MediaType.JSON_UTF_8.toString())
-                .withBody(JsonNodeFactory.instance.objectNode()
+        Response response = server.stubRequest(postJsonRequest(
+                wm.url(ADD_USER_URL),
+                JsonNodeFactory.instance.objectNode()
                         .put("id", UUID.randomUUID().toString())
                         .put("username", "root")
-                        .put("role", "admin")
-                        .toString()
-                        .getBytes(StandardCharsets.UTF_8))
-                .build());
+                        .put("role", "admin")));
 
         assertThat(response.getStatus())
                 .as("response should be successful, got body \"%s\"", response.getBodyAsString())
-                .isEqualTo(201);
+                .isEqualTo(HttpStatus.SC_CREATED);
     }
 
     @Test
     void testRequestBodyMissesRequiredProperties() {
         wm.stubFor(post(ADD_USER_URL)
-                .willReturn(aResponse()
-                        .withStatus(201)));
+                .willReturn(created()));
 
-        Response response = server.stubRequest(ImmutableRequest.create()
-                .withMethod(RequestMethod.POST)
-                .withAbsoluteUrl(wm.url(ADD_USER_URL))
-                .withHeader(CONTENT_TYPE_HEADER, MediaType.JSON_UTF_8.toString())
-                .withBody(JsonNodeFactory.instance.objectNode()
-                        .toString()
-                        .getBytes(StandardCharsets.UTF_8))
-                .build());
+        Response response = server.stubRequest(postJsonRequest(wm.url(ADD_USER_URL), "{}"));
 
         assertResponseFailedBecauseOfValidation(response);
         assertThat(response.getBodyAsString()).contains("Object has missing required properties");
@@ -113,23 +100,17 @@ class ValidationResponseTransformerTest {
     @Test
     void testRequestBodyHasInvalidProperties() {
         wm.stubFor(post(ADD_USER_URL)
-                .willReturn(aResponse()
-                        .withStatus(201)));
+                .willReturn(created()));
 
-        Response response = server.stubRequest(ImmutableRequest.create()
-                .withMethod(RequestMethod.POST)
-                .withAbsoluteUrl(wm.url(ADD_USER_URL))
-                .withHeader(CONTENT_TYPE_HEADER, MediaType.JSON_UTF_8.toString())
-                .withBody(JsonNodeFactory.instance.objectNode()
+        Response response = server.stubRequest(postJsonRequest(
+                wm.url(ADD_USER_URL),
+                JsonNodeFactory.instance.objectNode()
                         .put("id", "test")
                         .put("username", "toolongusername")
                         .put("name", "x")
                         .put("dob", "invalid date string")
                         .put("role", "unknown")
-                        .put("extra", "extra")
-                        .toString()
-                        .getBytes(StandardCharsets.UTF_8))
-                .build());
+                        .put("extra", "extra")));
 
         assertResponseFailedBecauseOfValidation(response);
         assertAll(
@@ -146,29 +127,21 @@ class ValidationResponseTransformerTest {
     void testRequestHasRequiredQueryStringParameter() {
         wm.stubFor(delete(urlPathEqualTo(DELETE_USER_URL))
                 .withQueryParam("soft", equalTo("true"))
-                .willReturn(aResponse()
-                        .withStatus(204)));
+                .willReturn(noContent()));
 
-        Response response = server.stubRequest(ImmutableRequest.create()
-                .withMethod(RequestMethod.DELETE)
-                .withAbsoluteUrl(wm.url(DELETE_USER_URL + "?soft=true"))
-                .build());
+        Response response = server.stubRequest(deleteRequest(wm.url(DELETE_USER_URL + "?soft=true")));
 
         assertThat(response.getStatus())
                 .as("response should be successful, got body \"%s\"", response.getBodyAsString())
-                .isEqualTo(204);
+                .isEqualTo(HttpStatus.SC_NO_CONTENT);
     }
 
     @Test
     void testRequestMissesRequiredQueryStringParameter() {
         wm.stubFor(delete(urlPathEqualTo(DELETE_USER_URL))
-                .willReturn(aResponse()
-                        .withStatus(204)));
+                .willReturn(noContent()));
 
-        Response response = server.stubRequest(ImmutableRequest.create()
-                .withMethod(RequestMethod.DELETE)
-                .withAbsoluteUrl(wm.url(DELETE_USER_URL))
-                .build());
+        Response response = server.stubRequest(deleteRequest(wm.url(DELETE_USER_URL)));
 
         assertResponseFailedBecauseOfValidation(response);
         assertThat(response.getBodyAsString()).contains("Query parameter 'soft' is required on path '/users/{userId}' but not found in request.");
@@ -177,38 +150,27 @@ class ValidationResponseTransformerTest {
     @Test
     void testResponseHasRequiredProperties() {
         wm.stubFor(get(GET_USERS_URL)
-                .willReturn(aResponse()
-                        .withHeader("Content-Type", MediaType.JSON_UTF_8.toString())
-                        .withJsonBody(JsonNodeFactory.instance.arrayNode()
+                .willReturn(jsonResponse(
+                        JsonNodeFactory.instance.arrayNode()
                                 .add(JsonNodeFactory.instance.objectNode()
                                         .put("id", UUID.randomUUID().toString())
                                         .put("username", "root")
-                                        .put("role", "admin")))
-                        .withStatus(200)));
+                                        .put("role", "admin")),
+                        HttpStatus.SC_OK)));
 
-        Response response = server.stubRequest(ImmutableRequest.create()
-                .withMethod(RequestMethod.GET)
-                .withAbsoluteUrl(wm.url(GET_USERS_URL))
-                .build());
+        Response response = server.stubRequest(getRequest(wm.url(GET_USERS_URL)));
 
         assertThat(response.getStatus())
                 .as("response should be successful, got body \"%s\"", response.getBodyAsString())
-                .isEqualTo(200);
+                .isEqualTo(HttpStatus.SC_OK);
     }
 
     @Test
     void testResponseMissesRequiredProperties() {
         wm.stubFor(get(GET_USERS_URL)
-                .willReturn(aResponse()
-                        .withHeader("Content-Type", MediaType.JSON_UTF_8.toString())
-                        .withJsonBody(JsonNodeFactory.instance.arrayNode()
-                                .add(JsonNodeFactory.instance.objectNode()))
-                        .withStatus(200)));
+                .willReturn(jsonResponse("[{}]", HttpStatus.SC_OK)));
 
-        Response response = server.stubRequest(ImmutableRequest.create()
-                .withMethod(RequestMethod.GET)
-                .withAbsoluteUrl(wm.url(GET_USERS_URL))
-                .build());
+        Response response = server.stubRequest(getRequest(wm.url(GET_USERS_URL)));
 
         assertResponseFailedBecauseOfValidation(response);
         assertThat(response.getBodyAsString()).contains("Object has missing required properties");
@@ -217,21 +179,17 @@ class ValidationResponseTransformerTest {
     @Test
     void testResponseHasInvalidProperties() {
         wm.stubFor(get(GET_USERS_URL)
-                .willReturn(aResponse()
-                        .withHeader("Content-Type", MediaType.JSON_UTF_8.toString())
-                        .withJsonBody(JsonNodeFactory.instance.arrayNode()
+                .willReturn(jsonResponse(
+                        JsonNodeFactory.instance.arrayNode()
                                 .add(JsonNodeFactory.instance.objectNode()
                                         .put("id", "test")
                                         .put("username", "toolongusername")
                                         .put("name", "x")
                                         .put("dob", "invalid date string")
-                                        .put("role", "unknown")))
-                        .withStatus(200)));
+                                        .put("role", "unknown")),
+                        HttpStatus.SC_OK)));
 
-        Response response = server.stubRequest(ImmutableRequest.create()
-                .withMethod(RequestMethod.GET)
-                .withAbsoluteUrl(wm.url(GET_USERS_URL))
-                .build());
+        Response response = server.stubRequest(getRequest(wm.url(GET_USERS_URL)));
 
         assertResponseFailedBecauseOfValidation(response);
         assertAll(
@@ -246,14 +204,9 @@ class ValidationResponseTransformerTest {
     @Test
     void testResponseHasInvalidStatusCode() {
         wm.stubFor(get(GET_USERS_URL)
-                .willReturn(aResponse()
-                        .withHeader("Content-Type", MediaType.JSON_UTF_8.toString())
-                        .withStatus(404)));
+                .willReturn(jsonResponse("", HttpStatus.SC_NOT_FOUND)));
 
-        Response response = server.stubRequest(ImmutableRequest.create()
-                .withMethod(RequestMethod.GET)
-                .withAbsoluteUrl(wm.url(GET_USERS_URL))
-                .build());
+        Response response = server.stubRequest(getRequest(wm.url(GET_USERS_URL)));
 
         assertResponseFailedBecauseOfValidation(response);
         assertThat(response.getBodyAsString()).contains("Response status 404 not defined for path '" + GET_USERS_URL + "'.");
@@ -262,14 +215,10 @@ class ValidationResponseTransformerTest {
     @Test
     void testUnknownPath() {
         wm.stubFor(get(UrlPattern.ANY)
-                .willReturn(aResponse()
-                        .withStatus(204)));
+                .willReturn(noContent()));
 
         String unknownPath = "/unknown";
-        Response response = server.stubRequest(ImmutableRequest.create()
-                .withMethod(RequestMethod.GET)
-                .withAbsoluteUrl(wm.url(unknownPath))
-                .build());
+        Response response = server.stubRequest(getRequest(wm.url(unknownPath)));
 
         assertResponseFailedBecauseOfValidation(response);
         assertThat(response.getBodyAsString()).contains("No API path found that matches request '" + unknownPath + "'.");
@@ -278,34 +227,24 @@ class ValidationResponseTransformerTest {
     @Test
     void testUnmatchedResponse() {
         wm.stubFor(get("/test")
-                .willReturn(aResponse()
-                        .withStatus(204)));
+                .willReturn(noContent()));
 
-        Response response = server.stubRequest(ImmutableRequest.create()
-                .withMethod(RequestMethod.GET)
-                .withAbsoluteUrl(wm.url("/not-test"))
-                .build());
+        Response response = server.stubRequest(getRequest(wm.url("/not-test")));
 
-        assertThat(response.getStatus()).isEqualTo(404);
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.SC_NOT_FOUND);
     }
 
     @Test
-    void testCustomResponseCodeOnValidationFailure() {
+    void testGlobalCustomResponseCodeOnValidationFailure() {
         System.setProperty("openapi_validation_failure_status_code", "599");
-        WireMockServer wm = new WireMockServer(wireMockConfig()
-                .httpServerFactory(factory)
-                .extensions(new ValidationResponseTransformer()));
+        WireMockServer wm = new WireMockServer(getWireMockConfiguration());
 
         wm.stubFor(get(UrlPattern.ANY)
-                .willReturn(aResponse()
-                        .withStatus(204)));
+                .willReturn(noContent()));
 
         DirectCallHttpServer server = factory.getHttpServer();
 
-        Response response = server.stubRequest(ImmutableRequest.create()
-                .withMethod(RequestMethod.GET)
-                .withAbsoluteUrl(wm.url("/test"))
-                .build());
+        Response response = server.stubRequest(getRequest(wm.url("/test")));
 
         assertResponseFailedBecauseOfValidation(response, 599);
     }
@@ -318,38 +257,34 @@ class ValidationResponseTransformerTest {
     }
 
     @Test
-    void testIgnoreSpecificErrors() {
+    void testGloballyIgnoreSpecificErrors() {
         System.setProperty("openapi_validation_ignore_errors", "validation.request.body.schema.required,validation.response.status.unknown");
 
-        WireMockServer wm = new WireMockServer(wireMockConfig()
-                .httpServerFactory(factory)
-                .extensions(new ValidationResponseTransformer()));
+        WireMockServer wm = new WireMockServer(getWireMockConfiguration());
 
         DirectCallHttpServer server = factory.getHttpServer();
 
         wm.stubFor(post(ADD_USER_URL)
-                .willReturn(aResponse()
-                        .withStatus(200)));
+                .willReturn(jsonResponse("{}", HttpStatus.SC_OK)));
 
-        Response response = server.stubRequest(ImmutableRequest.create()
-                .withMethod(RequestMethod.POST)
-                .withAbsoluteUrl(wm.url(ADD_USER_URL))
-                .withHeader(CONTENT_TYPE_HEADER, MediaType.JSON_UTF_8.toString())
-                .withBody(JsonNodeFactory.instance.objectNode()
-                        .toString()
-                        .getBytes(StandardCharsets.UTF_8))
-                .build());
+        Response response = server.stubRequest(postJsonRequest(wm.url(ADD_USER_URL), "{}"));
 
         assertThat(response.getStatus())
                 .as("response should be successful, got body \"%s\"", response.getBodyAsString())
-                .isEqualTo(200);
+                .isEqualTo(HttpStatus.SC_OK);
     }
 
-    void assertResponseFailedBecauseOfValidation(final Response response) {
-        assertResponseFailedBecauseOfValidation(response, 500);
+    private static WireMockConfiguration getWireMockConfiguration() {
+        return wireMockConfig()
+                .httpServerFactory(factory)
+                .extensions(new ValidationResponseTransformer());
     }
 
-    void assertResponseFailedBecauseOfValidation(final Response response, final int statusCode) {
+    private static void assertResponseFailedBecauseOfValidation(final Response response) {
+        assertResponseFailedBecauseOfValidation(response, DEFAULT_VALIDATION_FAILURE_STATUS_CODE);
+    }
+
+    private static void assertResponseFailedBecauseOfValidation(final Response response, final int statusCode) {
         assertAll(
                 "Response failed because of OpenAPI validation",
                 () -> assertThat(response.getStatus()).isEqualTo(statusCode),
